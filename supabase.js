39412c0e903472433b38a698ee51e4b3c07d9d8d
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-07-14 12:30:54 +0300

    Update Supabase URL and key
    Updated Supabase URL and key for project configuration.

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'ضع_رابط_المشروع_هنا'
-const supabaseKey = 'ضع_المفتاح_هنا'
+const supabaseUrl = 'sb_publishable_WWO97Lmy0MkItdWcceMDMw_AgRgJKpN'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkb2hxenlheW51YWN6YmdlcHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5ODY2OTIsImV4cCI6MjA5OTU2MjY5Mn0.4rSs9voibdeFlf9lG6hrSY-tv_yAzugFWZ6Z0WMzFco'
 
 export const supabase = createClient(
   supabaseUrl,
