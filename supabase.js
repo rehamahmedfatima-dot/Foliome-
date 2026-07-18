fb6cd39b8ebe919d0d7a5228764ab4eecf2c3e8a
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-07-18 17:16:04 +0300

    Rename supabase variable to sb for brevity

--- supabase.js
+++ supabase.js
@@ -2,5 +2,5 @@
 const SUPABASE_URL = 'https://cdohqzyaynuaczbgepsh.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkb2hxenlheW51YWN6YmdlcHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5ODY2OTIsImV4cCI6MjA5OTU2MjY5Mn0.4rSs9voibdeFlf9lG6hrSY-tv_yAzugFWZ6Z0WMzFco';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const PORTFOLIO_DOC_ID = 'main';
